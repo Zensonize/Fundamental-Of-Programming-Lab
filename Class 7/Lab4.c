c45b66e3b482254ea6662862617fcdb93def1982
Zensonize <krittametk@hotmail.com>
2017-10-02 12:18:50 +0700

    V1.1 (unsimplified)

--- Class 7/Lab4.c
+++ Class 7/Lab4.c
@@ -8,33 +8,42 @@ int main (){
         scanf("%d",&a);
         if(a<0) break;
         data[index] = a;
-        index++;
-    }
-    mode[0][modeindex] = data[0];
-    mode[1][modeindex] = 1;
-    modeindex++;
-    printf("index: %d\n",index);
-    for(i=1;i<index;i++){
-        printf("next data :%d \n\n",i);
-        for(j=0;j<modeindex;j++){
-            printf("\tChecking data:%d with mode:%d\n",i,j);
-            if(data[i]==mode[0][j]){
-                mode[1][j]++;
-                printf("found duplicated data\n");
-                //continue;
-                break;
-            }
-            else if(j == modeindex-1){
-                mode[0][modeindex] = data[i];
-                mode[1][modeindex] = 1;
-                modeindex++;
-                printf("No relevant data\n");
-                break;
+        if(index == 0){
+            mode[0][modeindex] = data[0];
+            mode[1][modeindex] = 1;
+            modeindex++;
+        }
+        if(index!=0){
+            for(j=0;j<modeindex;j++){
+                printf("\tChecking data:%d with mode:%d\n",index,j);
+                if(data[index]==mode[0][j]){
+                    mode[1][j]++;
+                    printf("found duplicated data\n");
+                    //continue;
+                    break;
+                }
+                else if(j == modeindex-1){
+                    mode[0][modeindex] = data[index];
+                    mode[1][modeindex] = 1;
+                    modeindex++;
+                    printf("No relevant data\n");
+                    break;
+                }
             }
         }
+        index++;
+        
     }
-
-    printf("modeindex: %d\n",modeindex);
+    
+    //< for debugging >
+    
+    for(i=0;i<modeindex;i++){
+        printf("%d ",mode[0][i]);
+    }
+    printf("\n");
+    for(i=0;i<modeindex;i++){
+        printf("%d ",mode[1][i]);
+    }    
 
     // <find max mode>
     int maxmode =0,maxmodeid,duplicate = 1;
@@ -50,17 +59,6 @@ int main (){
             break;
         }
     }
-    
     if(duplicate==0) printf("Mode is : %d\n",mode[0][maxmodeid]);
     else printf("no mode\n");
-    // < for debugging >
-    for(i=0;i<modeindex;i++){
-        printf("%d ",mode[0][i]);
-    }
-    printf("\n");
-    for(i=0;i<modeindex;i++){
-        printf("%d ",mode[1][i]);
-    }    
-
-    
 }
