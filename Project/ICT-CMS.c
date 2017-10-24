1d14310682809bad350f5754c4cb0cac43f547d4
Zensonize <krittametk@hotmail.com>
2017-10-24 06:04:49 -0700

    Added Main menu, routed to login and registeration page

--- Project/ICT-CMS.c
+++ Project/ICT-CMS.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+void Login(){
+
+}
+
+void URegist(){
+
+}
+
+void MMenu_routing (char choice){
+    if(choice == '1') URegist();
+    else if(choice == '2') Login();
+}
+
+void MMenu(){
+    char Mchoice;
+    printf("===============================\n");
+    printf("ICT Contact Management Systems\n");
+    printf("===============================\n");
+    printf("[1] User Registration\n");
+    printf("[2] Login\n");
+    printf("[0] Exit\n");
+    printf("-----------------------\n");
+    printf("Enter the choice : ");
+    while(1){
+        scanf(" %c",&Mchoice);
+        if(Mchoice == '1' || Mchoice == '2' || Mchoice == '0') break;
+    }
+    MMenu_routing(Mchoice);
+}
 
 int main() {
-    
+    MMenu();
+    return 0;
 }
