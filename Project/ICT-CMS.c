55785340e40b52c8796692d036b6d92d54d40e3d
Zensonize <krittametk@hotmail.com>
2017-10-24 06:54:43 -0700

    Added ID and Phone number in registration Form, Added Error handling for ID and Phone number

--- Project/ICT-CMS.c
+++ Project/ICT-CMS.c
@@ -1,9 +1,68 @@
 #include <stdio.h>
+int Regis_numErrorhandling (int value,int NO){
+
+    if(NO == 0){
+        int ID[7],index=0,val_cache;
+        val_cache = value;
+        while(val_cache>0){
+            ID[index++] = val_cache%10;
+            val_cache /= 10;
+        }
+        if(index != 7) {
+            printf("[ERROR] You can use only 7 digits for ID\n");
+            return 0;
+        }
+        else return 1;
+    } 
+    else{
+        int Phone[10],index=0,val_cache;
+        val_cache = value;
+        while(val_cache>0){
+            Phone[index++] = val_cache%10;
+            val_cache /= 10;
+        }
+        if(index == 10 && Phone[9] != 0){
+            printf("[ERROR] Phone number can't start with %d\n",Phone[9]);
+            return 0;
+        }
+        else if(index != 9) {
+            printf("[ERROR] You can use only 10 digits for phone number\n");
+            return 0;
+        }
+        else return 1;
+    }
+}
+
+void CScreen() {
+    printf("\n\n\n\n\n");
+}
+
 void Login(){
 
 }
 
 void URegist(){
+    int ID,Phone,Email,PW;
+    char First[20],Last[30];
+    int iscorrect=2;
+    CScreen();
+    printf(" [Registration]\n\n");
+    printf("Please enter the following information\n");
+
+    do{
+        iscorrect == 2 ? printf("ID: ") : printf("[Re-type] ID: ");
+        scanf("%d",&ID);
+        iscorrect = Regis_numErrorhandling (ID,0);
+    }while(iscorrect == 0);
+
+    //for First-Last Name
+
+    iscorrect = 2;
+    do{
+        iscorrect == 2 ? printf("Phone Number: ") : printf("[Re-type] Phone Number: ");
+        scanf("%d",&Phone);
+        iscorrect = Regis_numErrorhandling (Phone,1);
+    }while(iscorrect == 0);
 
 }
 
